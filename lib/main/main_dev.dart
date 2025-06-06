90e2a67b2a3a8b9bfae14fa152b2566855116f42
shehan187 <auburn@mufin.co.kr>
2025-06-06 11:39:40 +0530

    feat: lesson order

--- lib/main/main_dev.dart
+++ lib/main/main_dev.dart
@@ -7,6 +7,21 @@ import 'package:clean_arch_test1/main/bootstrap/bootstrap.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// the order of the teaching material is:
+// flavors
+// entry points
+// bootstrap
+// app folder
+// global blocs and widgets
+// actual app
+// folder structure of the clean architecture
+// core folder
+// presentation folder
+// domain folder
+// data folder
+// code the login flow
+// code the notes flow
+
 void main() {
   bootstrap(({
     required SharedPreferences sharedPreferences,
